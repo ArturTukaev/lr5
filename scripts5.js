e6a3fa67ef328e2b2bb8faf8fcef7b979aae387e
ArturTukaev <80099400+ArturTukaev@users.noreply.github.com>
2022-11-06 16:27:54 +0500

    LR5

---
+++ scripts5.js
@@ -0,0 +1,3 @@
+$(".nav_data .add_btn").click(function() {
+  $("table.Table tbody").append('<tr data-id="" data-new="1"><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td></tr>');
+});
